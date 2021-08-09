910fc8fa8cf3d0d644f0b91e6e0d486cba5d61fa
dan <dabdullaev@miu.edu>
2021-08-09 12:42:28 -0500

    Delete a note

--- src/components/NoteDetails.js
+++ src/components/NoteDetails.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { useHistory } from "react-router-dom";
 import NotesService from "../services/NotesService";
 
 const NoteDetails = () => {
     const {id} = useParams();
     const[currentNote, setCurrentNote] = useState('');
+    const history = useHistory();
 
     useEffect(() => {
         NotesService.get(id)
@@ -12,9 +14,17 @@ const NoteDetails = () => {
                 setCurrentNote(note.data);
             })
             .catch(error => {
-                console.log('Something went wrong', error);
+                console.log('Something went wrong. ', error);
             })
     }, []);
+
+    const handleDelete = () => {
+        NotesService.remove(id).then(note => {
+            history.push("/");
+        }).catch(error => {
+            console.log('Could not delete the note. ', error);
+        })
+    }
 
     return (
         <div className="note-details main-content">
@@ -26,6 +36,7 @@ const NoteDetails = () => {
                 </div>
                 <div className="mb-3">{currentNote.body}</div>
             </article>
+            <button onClick={handleDelete}>Delete</button>
         </div>
     );
 }
